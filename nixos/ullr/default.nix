ad04fa87a3b37e660bed6552ec6e01bdbc9d0963
Daniel Ramirez <dramirez@quickbase.com>
2024-12-15 12:45:43 +0000

    added os-prober

--- nixos/ullr/default.nix
+++ nixos/ullr/default.nix
@@ -86,6 +86,7 @@ in
       efiSupport = true;
       #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
       device = "nodev";
+      useOSProber = true;
     };
   };
   boot.plymouth = {
