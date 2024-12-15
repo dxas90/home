06dfa544f938f42e7767d39dadf604504cd88cc8
Daniel Ramirez <dramirez@quickbase.com>
2024-12-15 12:09:13 +0000

    more ssh

--- home-manager/daniel/default.nix
+++ home-manager/daniel/default.nix
@@ -161,9 +161,9 @@ in
      # https://chris.beams.io/posts/git-commit/
     '';
     ".gitignore".source = ./.gitignore;
-    ".config/environment.d/ssh-agent.conf".text = ''
-    SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent
-    '';
+    # ".config/environment.d/ssh-agent.conf".text = ''
+    # SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent
+    # '';
     ".config/starship.toml".source = ./starship.toml;
     ".config/nvim" = {
         source = ./nvim;
