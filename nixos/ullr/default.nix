5bebf2bd928dee54e0d19dd4eb93c307977d9ab5
Daniel Ramirez <dramirez@quickbase.com>
2024-12-15 12:00:35 +0000

    more SSH

--- nixos/ullr/default.nix
+++ nixos/ullr/default.nix
@@ -284,6 +284,12 @@ in
   };
   environment.interactiveShellInit = ''
     alias vim='nvim'
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+        ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+    fi
+    if [[ ! "$SSH_AUTH_SOCK" ]]; then
+        source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+    fi
   '';
 
   programs.neovim.enable = true;
