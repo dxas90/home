a4ae577fc17ec03312712962e552bc481d8d6ce7
Daniel Ramirez <dramirez@quickbase.com>
2025-01-07 15:29:51 +0000

    more changes

--- nixos/ullr/default.nix
+++ nixos/ullr/default.nix
@@ -74,13 +74,13 @@ in
   ###############################################
   boot = {
     kernelParams = [
-      "quiet"
-      "splash"
       "boot.shell_on_fail"
       "loglevel=3"
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+      "quiet"
+      "splash"
     ];
     consoleLogLevel = 0;
     initrd.verbose = false;
