10f827235d893586e80b8e21dea1e447df24d1b0
Daniel Ramirez <dramirez@quickbase.com>
2025-01-13 14:05:17 +0000

    testing var

--- home-manager/daniel/secrets.nix
+++ home-manager/daniel/secrets.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }:
 let
-  ageKeyFile = ".config/sops/age/keys.txt";
+  ageKeyFile = "${home.homeDirectory}/.config/sops/age/keys.txt";
   # ageKeyFile = "${config.users.users.daniel.home}/.config/sops/age/keys.txt";
 in
 {
@@ -8,7 +8,7 @@ in
     sops = {
       age.generateKey = true;
       age.keyFile = ageKeyFile;
-      age.sshKeyPaths = [ ".ssh/automation.pub" ];
+      age.sshKeyPaths = [ "${home.homeDirectory}/.ssh/automation.pub" ];
       # age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       defaultSopsFile = ./example.sops.yaml;
       secrets."myservice/my_subdir/my_secret" = {
