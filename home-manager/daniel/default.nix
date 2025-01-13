5a75f2ac72e293824ccb3936c52c20f051dc43f3
Daniel Ramirez <dramirez@quickbase.com>
2025-01-13 16:13:06 +0000

    more rework

--- home-manager/daniel/default.nix
+++ home-manager/daniel/default.nix
@@ -239,10 +239,10 @@
   programs.home-manager.enable = true;
 
   sops = {
-    age.keyFile = "/home/daniel/.config/sops/age/keys.txt"; # must have no password!
+    age.keyFile = "${home.homeDirectory}/.config/sops/age/keys.txt"; # must have no password!
     # It's also possible to use a ssh key, but only when it has no password:
-    age.sshKeyPaths = [ "/home/daniel/.ssh/automation.pub" ];
-    defaultSopsFile = ./example.sops.yaml;
+    age.sshKeyPaths = [ "${home.homeDirectory}/.ssh/automation.pub" ];
+    defaultSopsFile = ./secrets.sops.yaml;
     secrets."myservice/my_subdir/my_secret" = {
       mode = "0440";
       # owner = config.users.users.daniel.name;
