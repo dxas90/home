f58c85382aed13cac9aa69daec1c37047e1075f6
Daniel Ramirez <dramirez@quickbase.com>
2025-01-13 14:04:09 +0000

    changed ssh key

--- home-manager/daniel/secrets.nix
+++ home-manager/daniel/secrets.nix
@@ -1,16 +1,20 @@
 { pkgs, config, ... }:
 let
   ageKeyFile = ".config/sops/age/keys.txt";
+  # ageKeyFile = "${config.users.users.daniel.home}/.config/sops/age/keys.txt";
 in
 {
   config = {
     sops = {
       age.generateKey = true;
       age.keyFile = ageKeyFile;
-      age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      age.sshKeyPaths = [ ".ssh/automation.pub" ];
+      # age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       defaultSopsFile = ./example.sops.yaml;
       secrets."myservice/my_subdir/my_secret" = {
         mode = "0440";
+        # owner = config.users.users.daniel.name;
+        # group = config.users.users.daniel.group;
       };
     };
   };
