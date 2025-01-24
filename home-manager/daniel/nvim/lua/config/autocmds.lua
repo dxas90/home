7cb05b008eeb30f13d98da1f8959044f5e5f4652
Daniel Ramirez <dxas90@gmail.com>
2025-01-24 12:47:42 +0200

    changed format

--- home-manager/daniel/nvim/lua/config/autocmds.lua
+++ home-manager/daniel/nvim/lua/config/autocmds.lua
@@ -8,9 +8,12 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_create_autocmd({"BufEnter"}, {
     callback = function(event)
-        local title = "vim"
+        local root_folder = vim.fs.find({".git", "package.json", ".gitignore"}, { upward = true, stop = vim.loop.os_homedir() })[1]
+        local root_name = root_folder and vim.fs.basename(vim.fs.dirname(root_folder)) or ""
+
+        local title = root_name
         if event.file ~= "" then
-            title = string.format("vim: %s", vim.fs.basename(event.file))
+            title = string.format("%s: %s", root_name, vim.fs.basename(event.file))
         end
 
         vim.fn.system({"wezterm", "cli", "set-tab-title", title})
