242f481e3400e762802d32458ade11c5f09afc08
Daniel Ramirez <dxas90@gmail.com>
2025-01-25 16:42:52 +0200

    testing more wezterm

--- home-manager/daniel/config/wezterm.lua
+++ home-manager/daniel/config/wezterm.lua
@@ -38,19 +38,7 @@ config.window_background_gradient = {
 --}
 config.front_end = 'WebGpu'
 
-config.use_fancy_tab_bar = false
-
-local function get_current_working_dir(tab)
-    local current_dir = tab.active_pane.current_working_dir
-    local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
-    return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\\\])(.*)", "%2")
-end
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-    local title = string.format(" %s  %s  ", "❯", get_current_working_dir(tab))
-    return {
-        { Text = title },
-    }
-end)
+--config.use_fancy_tab_bar = false
 
 return config
+
