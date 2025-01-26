21e8064ec4170f2bf07ebd54794589312ec9e6d4
Daniel Ramirez <dxas90@gmail.com>
2025-01-26 16:10:46 +0200

    new tmux settings

--- home-manager/manjaro/default.nix
+++ home-manager/manjaro/default.nix
@@ -63,9 +63,9 @@ in
         # Mouse works as expected
         set-option -g mouse on
         # easy-to-remember split pane commands
-        #bind | split-window -h -c "#{pane_current_path}"
-        #bind - split-window -v -c "#{pane_current_path}"
-        #bind c new-window -c "#{pane_current_path}"
+        bind '%' split-window -h -c "#{pane_current_path}"
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
       '';
     };
     wezterm = {
