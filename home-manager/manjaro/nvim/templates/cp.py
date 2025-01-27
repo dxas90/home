ef1e001941ed305adccff3a951c20da74a2e6ee0
Daniel Ramirez <dxas90@gmail.com>
2025-01-27 14:43:05 +0200

    testing nvim templates

---
+++ home-manager/manjaro/nvim/templates/cp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# {{_author_}} <{{_email_}}>
+# Copyright (c) {{_date_}}
+# When I wrote this, only God and I understood what I was doing
+# Now, God only knows
+from __future__ import print_function
+import sys
+
+getline = lambda: sys.stdin.readline().strip()
+
+cache = {}
+def _cache(n,fun = int, cache = cache):
+    try:
+        return cache[n]
+    except KeyError:
+        cache[n] = fun(n)
+        return cache[n]
+
+MAXV = 1000
+
+if __name__ == '__main__':
+    try:
+        if 'MY_FLAG' in sys.argv:
+            sys.stdin=open('/tmp/input')
+      {{_cursor_}}
+    except Exception:
+        exit
+
+
+
